e91f76bf07e8a0997118c85deeff84d7725cb6bb
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-03 20:30:37 +0300

    feat: add global stats bar component

---
+++ frontend/components/StatsBar.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function StatsBar({ totalVotes, totalPolls }: { totalVotes: number; totalPolls: number }) {
+  return (
+    <div className="flex justify-center gap-6 mb-8 text-sm text-gray-400">
+      <span>{totalPolls} polls</span>
+      <span>{totalVotes.toLocaleString()} total votes</span>
+    </div>
+  );
+}
